7a138c7f7972005a24c9320cae72f1742845a038
mossi <mossi.hashemi@gmail.com>
2019-04-02 12:32:15 +0200

    14.Basics of middleware and uses

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,9 +1,23 @@
+import { runInNewContext } from "vm";
+
 // here we create all the routes we need,
 //in order to create a route, we'll create a function call "routes"
 const routes = (app) => {
     app.route('/contact')
-    .get((req, res) => 
-    res.send('GET request successfull !!! '))
+    .get((req, res, next) => {
+        /* -------- MiddleWare -----------
+         middleWares are simply functions that have access to 
+         the request and response object in out express application, 
+         it can run their code there, it can make changes to the request response objects, 
+         can end them, can call on other function and stack using next & etc.
+         (we can anythings we want as middleWare in our endpoits, 
+         it could also be at third-party library where you want to do sth special) */
+        console.log(`Request from : ${req.originalUrl}`)
+        console.log(`Request type : ${req.method}`)
+        next();
+    }, (req, res, next) =>{
+        res.send('GET request successfull !!! ');
+    })
 
     .post((req, res) => 
     res.send('POST request successfull !!! '));
